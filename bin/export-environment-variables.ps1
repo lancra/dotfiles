edf53b729f31d8ab5363e9b26ff4c9132c31bba4
Lance Craig <lancra@outlook.com>
2024-03-03 07:29:13 -0500

    env: use expanded paths on machine
    After updating environment variable paths to use un-expanded variants
    (e.g. "%PROGRAMFILES%\Git\cmd"), I found that installers and programs
    did not recognize the variables, and would re-add or modify them.
    
    The variables have been converted back to the expanded format and the
    script has been updated to convert them back for specified prefixes. A
    side benefit is that the %SYSTEMROOT% variable that was added to the
    user path can be removed, since its only purpose was to trigger the
    Windows dialog to recognize the text as an array.
    
    Link: https://superuser.com/a/1594989

--- bin/export-environment-variables.ps1
+++ bin/export-environment-variables.ps1
@@ -6,6 +6,61 @@ param (
     [string]$Name = 'default'
 )
 
+$prefixVariableNames = @(
+    'LOCALAPPDATA',
+    'APPDATA',
+    'USERPROFILE',
+    'DEVUSERPROFILE',
+    'HOME',
+    'DEV_HOME',
+    'SYSTEMROOT',
+    'PROGRAMFILES(X86)',
+    'PROGRAMFILES'
+)
+
+$prefixVariables = [ordered]@{}
+$prefixVariableNames |
+    ForEach-Object {
+        $prefixVariables[$_] = [System.Environment]::GetEnvironmentVariable($_)
+    }
+
+function Set-PathPrefix {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory)]
+        [System.Collections.Specialized.OrderedDictionary]$Prefixes,
+        [Parameter(Mandatory)]
+        [string]$Path,
+        [switch]$AllowExact
+    )
+    begin {
+        $pathRegex = '[a-zA-Z]:[\\\/]'
+    }
+    process {
+        if ($Path -notmatch $pathRegex) {
+            Write-Verbose "$Path does not match the format for path prefixing."
+            return $Path
+        }
+
+        foreach ($prefix in $Prefixes.GetEnumerator()) {
+            $prefixPath = $prefix.Value
+            Write-Verbose "  Testing Path=$Path, Prefx=$prefixPath"
+
+            if ($Path -like "$prefixPath*" -and ($Path -ne $prefixPath -or $AllowExact)) {
+                $prefixName = $prefix.Key
+                $pathSuffix = $Path.Substring($prefixPath.Length)
+                $resultPath = "%$prefixName%$pathSuffix"
+
+                Write-Verbose "    Match=$resultPath"
+                return $resultPath
+            }
+        }
+
+        $Path
+    }
+}
+
 function Get-EnvironmentVariable {
     [CmdletBinding()]
     [OutputType([ordered])]
@@ -32,7 +87,19 @@ function Get-EnvironmentVariable {
 
                 $value = $registryKey.GetValue($_, '', $registryValueOptions)
                 if ($key -eq 'PATH') {
-                    $value = $value -split ';' -ne ''
+                    Write-Verbose 'Processing PATH variable parts...'
+                    $valueParts = $value -split ';' -ne ''
+                    $prefixedParts = @()
+
+                    foreach ($part in $valueParts) {
+                        Write-Verbose "Processing $part from PATH..."
+                        $prefixedParts += Set-PathPrefix -Prefixes $prefixVariables -Path $part -AllowExact
+                    }
+
+                    $value = $prefixedParts
+                } else {
+                    Write-Verbose "Processing $key variable..."
+                    $value = Set-PathPrefix -Prefixes $prefixVariables -Path $value
                 }
 
                 $environmentVariables[$key] = $value
