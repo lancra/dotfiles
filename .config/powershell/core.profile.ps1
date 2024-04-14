cd54e9be0662f9c7aa42b022cd9602ca07f9ee36
Lance Craig <lancra@outlook.com>
2024-04-14 09:19:23 -0400

    pwsh: add machine export alias

--- .config/powershell/core.profile.ps1
+++ .config/powershell/core.profile.ps1
@@ -11,6 +11,7 @@ Set-PSReadLineKeyHandler -Key '*,y' -BriefDescription 'global yank' -ViMode Comm
 
 Import-Module HackF5.ProfileAlias
 Set-ProfileAlias l 'lsd -l #{:*}' -Bash -Force | Out-Null
+Set-ProfileAlias em 'export-machine.ps1' -Force | Out-Null
 
 Import-Module 'posh-git'
 oh-my-posh init pwsh --config "$env:XDG_CONFIG_HOME/powershell/lancra.omp.json" | Invoke-Expression
