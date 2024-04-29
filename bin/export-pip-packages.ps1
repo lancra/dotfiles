0d72fdcc10714aa9630af4a58909ecee2423cd66
Lance Craig <lancra@outlook.com>
2024-04-29 06:24:25 -0400

    pip: add homepage to package export
    The homepage is not being returned by the `pip show` command, so it's
    instead being pulled directly from the package metadata in the
    distribution information directory.

--- bin/export-pip-packages.ps1
+++ bin/export-pip-packages.ps1
@@ -5,13 +5,20 @@ $directoryPath = Join-Path -Path $env:XDG_CONFIG_HOME -ChildPath 'pip'
 New-Item -ItemType Directory -Path $directoryPath -Force | Out-Null
 
 $packagesPath = Join-Path $directoryPath -ChildPath 'packages.csv'
-$properties = @(
-    @{Name = 'Id'; Expression = { $_.name }}
-)
 
+$homepageGroupName = 'Homepage'
 & pip list --not-required --format json --verbose |
     ConvertFrom-Json |
-    Select-Object -Property $properties |
+    ForEach-Object {
+        $distInfoDirectory = Get-ChildItem -Path $_.location -Filter "$($_.name)*.dist-info"
+        $metadataPath = Join-Path -Path $distInfoDirectory.FullName -ChildPath 'METADATA'
+        $homepageMatch = Select-String -Path $metadataPath -Pattern "^Project-URL: Homepage, (?<$homepageGroupName>.*)"
+        $homepageMatchGroup = $homepageMatch.Matches.Groups | Where-Object -Property Name -EQ $homepageGroupName
+        @{
+            Id = $_.name
+            Homepage = $homepageMatchGroup.Value
+        }
+    } |
     Sort-Object -Property Id |
     ConvertTo-Csv -UseQuotes AsNeeded |
     Set-Content -Path $packagesPath
