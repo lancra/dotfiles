9f309a60ff71d012dc0742807bfd4c0ccfdd6a30
Lance Craig <lancra@outlook.com>
2024-05-26 23:03:35 -0400

    env: parallelize machine export

--- bin/export-machine.ps1
+++ bin/export-machine.ps1
@@ -14,23 +14,6 @@ param (
     [string]$Provider
 )
 begin {
-    function Publish-Export {
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory)]
-            [string]$Name,
-            [Parameter(Mandatory)]
-            [string]$Script,
-            [Parameter(Mandatory)]
-            [string]$Target
-        )
-        process {
-            Write-Host "Exporting $Name..." -NoNewline
-            & $Script -Target $Target
-            Write-Host 'Done'
-        }
-    }
-
     $beginLoading = "`e]9;4;3`a"
     $endLoading = "`e]9;4;0`a"
 
@@ -38,13 +21,12 @@ begin {
 }
 process {
     & "$env:XDG_CONFIG_HOME/env/get-providers.ps1" -Id $Provider |
-        ForEach-Object {
+        ForEach-Object -Parallel {
             if ($_.export) {
-                $name = "$($_.id) $($_.resource)"
                 $directory = "$env:XDG_CONFIG_HOME/$($_.id)"
                 $script = "$directory/export-$($_.resource).ps1"
                 $target = "$directory/$($_.resource).$($_.store)"
-                Publish-Export -Name $name -Script $script -Target $target
+                & $script -Target $target
             }
         }
 }
