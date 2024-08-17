74e2ba34b5517edcdd6490918c93a2ae906bdc0a
Lance Craig <lancra@outlook.com>
2024-08-16 21:20:25 -0400

    pip: harden package export
    Some packages have a different directory than their file name, so this
    script now uses the `pip show --files` command to find the METADATA
    file.

--- .config/pip/export-packages.ps1
+++ .config/pip/export-packages.ps1
@@ -8,9 +8,13 @@ $homepageGroupName = 'Homepage'
 & pip list --not-required --format json --verbose |
     ConvertFrom-Json |
     ForEach-Object {
-        $distInfoDirectory = Get-ChildItem -Path $_.location -Filter "$($_.name)*.dist-info"
-        $metadataPath = Join-Path -Path $distInfoDirectory.FullName -ChildPath 'METADATA'
-        $homepageMatch = Select-String -Path $metadataPath -Pattern "^Project-URL: Homepage, (?<$homepageGroupName>.*)"
+        $metadataRelativePath = & pip show --files $_.name |
+            Where-Object { $_ -match '.*.dist-info\\METADATA' } |
+            ForEach-Object { $_.Trim() } |
+            Select-Object -First 1
+        $metadataPath = Join-Path -Path $_.location -ChildPath $metadataRelativePath
+
+        $homepageMatch = Select-String -Path $metadataPath -Pattern "^Project-URL: Homepage, (?<$homepageGroupName>.*)|^Home-page: (?<$homepageGroupName>.*)"
         $homepageMatchGroup = $homepageMatch.Matches.Groups | Where-Object -Property Name -EQ $homepageGroupName
         [ordered]@{
             Id = $_.name
