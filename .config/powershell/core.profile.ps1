ca2aefa87a92938948851cfa3a7937c35763cd0f
Lance Craig <lancra@outlook.com>
2024-08-21 05:29:17 -0400

    pwsh: add recursive remove alias

--- .config/powershell/core.profile.ps1
+++ .config/powershell/core.profile.ps1
@@ -15,6 +15,7 @@ Set-ProfileAlias em 'export-machine.ps1' -Force | Out-Null
 Set-ProfileAlias um 'update-machine.ps1' -Force | Out-Null
 Set-ProfileAlias g 'git #{:*}' -Bash -Force | Out-Null
 Set-ProfileAlias l 'lsd -l #{:*}' -Bash -Force | Out-Null
+Set-ProfileAlias rmr 'Remove-Item -Path #{0} -Recurse' -Bash -Force | Out-Null
 Set-ProfileAlias wu 'winget upgrade #{:*}' -Bash -Force | Out-Null
 
 Import-Module 'posh-git'
