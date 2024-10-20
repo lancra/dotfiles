767a58c2cb35d90fd1dc14ef26bfc6abc6163af9
Lance Craig <lancra@outlook.com>
2024-10-20 07:55:04 -0400

    snips: support comparison of vim snippets

---
+++ .config/snippets/compare-vim-snippet.ps1
@@ -0,0 +1,50 @@
+using module ./snippets.psm1
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [Snippet]$Snippet,
+    [Parameter(Mandatory)]
+    [SnippetEditor]$Configuration
+)
+
+$directory = Resolve-Path -Path $Configuration.TargetDirectory
+
+$script:missingScope = $false
+$snippetContents = @{}
+$Snippet.Scope |
+    ForEach-Object {
+        $scope = $Configuration.ScopeOverrides.$_ ?? $_
+        $prefix = $Snippet.Prefix[0]
+        $snippetScopePath = Join-Path -Path $directory -ChildPath $scope -AdditionalChildPath "$prefix.snippets"
+        if (-not (Test-Path -Path $snippetScopePath)) {
+            Write-Error "The VIM snippet for $prefix in scope $_ could not be found."
+            $script:missingScope = $true
+            return
+        }
+
+        $content = Get-Content -Path $snippetScopePath
+        $contentStream = [System.IO.MemoryStream]::new([byte[]][char[]]($content -join ''))
+        $contentHashInfo = Get-FileHash -InputStream $contentStream -Algorithm SHA256
+        $contentHash = $contentHashInfo.Hash
+
+        if (-not $snippetContents.ContainsKey($contentHash)) {
+            $snippetContents[$contentHash] = @{
+                Scopes = @()
+                Content = $content
+            }
+        }
+
+        $snippetContents[$contentHash].Scopes += $_
+    }
+
+$snippetContents.GetEnumerator() |
+    ForEach-Object {
+        $scopesDisplay = $_.Value.Scopes -join ' / '
+        $displayName = "VIM ($scopesDisplay)"
+        $_.Value.Content | & bat --paging=never --file-name=$displayName
+    }
+
+if ($script:missingScope) {
+    exit 1
+}
