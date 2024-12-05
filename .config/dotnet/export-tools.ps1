43f97fdec7f0f9adbbc5505b66d77d88994400dd
Lance Craig <lancra@outlook.com>
2024-12-05 06:38:08 -0500

    dotnet: fix export for new sdk version
    The script now checks for the first-use sentinel file corresponding to
    the current SDK version, and adds it if not present. This prevents the
    welcome message from breaking the CLI output.

--- .config/dotnet/export-tools.ps1
+++ .config/dotnet/export-tools.ps1
@@ -4,6 +4,13 @@ param (
     [string]$Target
 )
 
+$sentinelPath = Join-Path -Path $env:HOME -ChildPath '.dotnet' -AdditionalChildPath "$(& dotnet --version).dotnetFirstUseSentinel"
+if (-not (Test-Path -Path $sentinelPath)) {
+    # If the first use sentinel for the current SDK version is not present, it prepends the output with a welcome message and breaks
+    # the export process.
+    New-Item -Path $sentinelPath | Out-Null
+}
+
 & dotnet tool list --global |
     ForEach-Object {
         if ($_.StartsWith('Package Id') -or $_.StartsWith('---')) {
