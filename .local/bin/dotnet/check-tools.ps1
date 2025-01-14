f8cd7850fe3e92fd09881c3538d9f5f1f4eef86f
Lance Craig <lancra@outlook.com>
2025-01-14 05:38:34 -0500

    dotnet: fix outdated tools check
    The dotnet-tools-outdated tool does not give any output when all tools
    are up-to-date as of a recent update. This script now outputs all tools,
    regardless of the version state, and only exports the tools where the
    current version is different from the available version.

--- .local/bin/dotnet/check-tools.ps1
+++ .local/bin/dotnet/check-tools.ps1
@@ -1,15 +1,17 @@
 [CmdletBinding()]
 param()
 
-& dotnet-tools-outdated --format json --noIndent |
+& dotnet-tools-outdated --format json --noIndent --outPkgRegardlessState |
     ConvertFrom-Json |
     Select-Object -ExpandProperty dotnet-tools-outdated |
     ForEach-Object {
+      if ($_.currentVer -ne $_.availableVer) {
         [ordered]@{
             provider = 'dotnet'
             id = $_.packageName
             current = $_.currentVer
             available = $_.availableVer
         }
+      }
     } |
     ConvertTo-Json -AsArray
