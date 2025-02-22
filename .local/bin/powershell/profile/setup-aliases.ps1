08b87b5fda2a0d8a2c367ee2b9d3351111e284c8
Lance Craig <lancra@outlook.com>
2025-02-22 07:04:12 -0500

    pwsh: restructure aliases
    - Convert array to object, where each property is the alias. This
      prevents accidental duplication.
    - Add group property to each alias and use as first sorting property.

--- .local/bin/powershell/profile/setup-aliases.ps1
+++ .local/bin/powershell/profile/setup-aliases.ps1
@@ -1,7 +1,7 @@
 Import-Module HackF5.ProfileAlias
 Get-Content -Path "$env:XDG_CONFIG_HOME/powershell/aliases.json" |
     ConvertFrom-Json |
+    ForEach-Object { $_.PSObject.Properties } |
     ForEach-Object {
-        $bash = [bool]::Parse($_.bash)
-        Set-ProfileAlias -Name $_.name -Command $_.command -Bash:$bash -Force | Out-Null
+        Set-ProfileAlias -Name $_.Name -Command $_.Value.command -Bash:$_.Value.bash -Force | Out-Null
     }
