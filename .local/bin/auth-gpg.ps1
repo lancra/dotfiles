7e763babab7a9957fa2f36d9f4cdcc2d5bc7b15e
Lance Craig <lancra@outlook.com>
2025-03-03 06:43:43 -0500

    gpg: add auth script

---
+++ .local/bin/auth-gpg.ps1
@@ -0,0 +1,105 @@
+[CmdletBinding()]
+param(
+    [switch] $SkipPasswordManager
+)
+
+Get-Command -Name gpg -ErrorAction Stop | Out-Null
+
+if (-not $SkipPasswordManager) {
+    Get-Command -Name bw -ErrorAction Stop | Out-Null
+}
+
+$filePath = "$env:TEMP/gpg-auth.$(New-Guid).txt"
+New-Item -ItemType File -Path $filePath | Out-Null
+
+function Reset-File {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string] $Path
+    )
+    process {
+        Write-Verbose 'Deleting temporary files.'
+        Remove-Item -Path $Path | Out-Null
+        Remove-Item -Path "$Path.gpg" | Out-Null
+    }
+}
+
+function Connect-Bitwarden {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param()
+    process {
+        Write-Verbose 'Checking authentication status for Bitwarden.'
+        $status = & bw login --check 2>&1
+        $authenticated = $status -eq 'You are logged in!'
+
+        if ($authenticated) {
+            Write-Verbose 'Unlocking Bitwarden.'
+            $message = & bw unlock
+        } else {
+            Write-Verbose 'Authenticating Bitwarden.'
+            $message = & bw login
+        }
+
+        $sessionLine = $message |
+            Where-Object { $_.StartsWith('>') } |
+            Select-Object -First 1
+
+        $sessionGroup = 'session'
+        $sessionLine |
+            Select-String -Pattern "`"(?<$sessionGroup>.*?)`"" |
+            Select-Object -ExpandProperty Matches |
+            Select-Object -ExpandProperty Groups |
+            Where-Object -Property Name -EQ $sessionGroup |
+            Select-Object -ExpandProperty Value
+    }
+}
+
+function Test-Gpg {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory)]
+        [string] $Path
+    )
+    process {
+        Write-Verbose 'Checking GPG for cached passphrase.'
+        $result = & gpg --pinentry-mode error --sign $Path 2>&1
+        return $null -eq $result
+    }
+}
+
+function Connect-Gpg {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [securestring] $Passphrase,
+
+        [Parameter(Mandatory)]
+        [string] $Path
+    )
+    process {
+        Write-Verbose 'Caching passphrase with GPG.'
+        $rawPasphrase = $Passphrase | ConvertFrom-SecureString -AsPlainText
+        & gpg --pinentry-mode loopback --passphrase $rawPasphrase --sign $Path
+    }
+}
+
+if (-not (Test-Gpg -Path $filePath)) {
+    if ($SkipPasswordManager) {
+        $passphrase = Read-Host -Prompt 'Passphrase' -AsSecureString
+    } else {
+        $bitwardenSession = Connect-Bitwarden
+
+        Write-Verbose 'Getting secret key passphrase from Bitwarden.'
+        $passphrase = & bw get --session $bitwardenSession password gpg |
+            ConvertTo-SecureString -AsPlainText
+
+        & bw lock | Out-Null
+    }
+
+    Connect-Gpg -Passphrase $passphrase -Path $filePath
+}
+
+Reset-File -Path $filePath
