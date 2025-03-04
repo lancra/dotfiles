70ebc49a083171270280db5b1248a595f6f6cac3
Lance Craig <lancra@outlook.com>
2025-03-04 06:45:34 -0500

    pwsh: update output of indent check
    Instead of a PowerShell object where the Path property gets cut off,
    this script now writes the indentation counts and paths.
    
    The indent counts also denote status via text color:
    
    - Green when the file contains a single, valid indentation.
    - Red when the file contains multiple or mixed indentations.
    - Magenta when the file contains other indentations, such as
      non-breaking spaces or form feed characters.
    
    Additionally, an All switch allows for the complete output of the
    search, which grants a better view into the overall indentation used for
    any file searched.

--- .local/bin/find-mixed-indents.ps1
+++ .local/bin/find-mixed-indents.ps1
@@ -7,7 +7,9 @@ param(
     [string] $IgnorePath,
 
     [Parameter()]
-    [int] $Depth = $null
+    [int] $Depth = $null,
+
+    [switch] $All
 )
 
 Get-Command -Name fd -ErrorAction Stop | Out-Null
@@ -52,22 +54,11 @@ function Test-TextFile {
     }
 }
 
-function Get-IndentCount {
-    [CmdletBinding()]
-    [OutputType([int])]
-    param(
-        [Parameter(Mandatory)]
-        [string] $Path,
-
-        [Parameter(Mandatory)]
-        [char] $Indent
-    )
-    process {
-        Select-String -Path $Path -Pattern "^$Indent" -ErrorAction SilentlyContinue |
-            Measure-Object |
-            Select-Object -ExpandProperty Count
-    }
-}
+$spacesIdentifier = 'Spaces'
+$tabsIdentifier = 'Tabs'
+$mixedIdentifier = 'Mixed'
+$otherIdentifier = 'Other'
+$identifiers = @($spacesIdentifier, $tabsIdentifier, $mixedIdentifier, $otherIdentifier)
 
 $targets |
     ForEach-Object {
@@ -75,14 +66,66 @@ $targets |
             return
         }
 
-        $spaceIndentCount = Get-IndentCount -Path $_ -Indent ' '
-        $tabIndentCount = Get-IndentCount -Path $_ -Indent "`t"
+        # Error action is set since some Windows system files can prevent reads and don't need to be checked anyways.
+        $groups = Select-String -Path $_ -Pattern '^\s+' -ErrorAction SilentlyContinue |
+            Select-Object -ExpandProperty Matches |
+            Select-Object -ExpandProperty Value |
+            ForEach-Object {
+                $hasSpaces = $_.Contains(' ')
+                $hasTabs = $_.Contains("`t")
 
-        if ($spaceIndentCount -ne 0 -and $tabIndentCount -ne 0) {
-            [PSCustomObject]@{
-                Path = $_
-                Spaces = $spaceIndentCount
-                Tabs = $tabIndentCount
+                if ($hasSpaces -and $hasTabs) {
+                    $mixedIdentifier
+                } elseif ($hasSpaces) {
+                    $spacesIdentifier
+                } elseif ($hasTabs) {
+                    $tabsIdentifier
+                } else {
+                    $otherIdentifier
+                }
+            } |
+            Group-Object
+
+        $invalidGroupCount = $groups |
+            Where-Object { $_.Name -eq $mixedIdentifier -or $_.Name -eq $otherIdentifier } |
+            Measure-Object |
+            Select-Object -ExpandProperty Count
+        if ($groups.Length -le 1 -and $invalidGroupCount -eq 0 -and -not $All) {
+            return
+        }
+
+        $outputSegments = $identifiers |
+            ForEach-Object {
+                $group = $groups |
+                    Where-Object -Property Name -EQ $_
+
+                $count = $group.Count ?? 0
+                $isOther = $_ -eq $otherIdentifier
+
+                $color = [System.Console]::ForegroundColor
+                if ($count -ne 0 -and $isOther) {
+                    $color = [System.ConsoleColor]::Magenta
+                } elseif ($count -ne 0 -and ($groups.Length -gt 1 -or $group.Name -eq $mixedIdentifier)) {
+                    $color = [System.ConsoleColor]::Red
+                } elseif ($count -ne 0 -and $groups.Length -eq 1) {
+                    $color = [System.ConsoleColor]::Green
+                }
+
+                $letter = $_[0]
+                $ticker = '{0:D4}' -f $count
+                $text = -not ($count -eq 0 -and $isOther) ? "$letter$ticker " : [string]::new(' ', 6)
+
+                @{
+                    Object = $text
+                    ForegroundColor = $color
+                }
             }
+
+        $outputSegments += @{ Object = ($_ -replace '\\', '/') }
+
+        for ($i = 0; $i -lt $outputSegments.Count; $i++) {
+            $segment = $outputSegments[$i]
+            $segment['NoNewline'] = $i -ne ($outputSegments.Count - 1)
+            Write-Host @segment
         }
     }
