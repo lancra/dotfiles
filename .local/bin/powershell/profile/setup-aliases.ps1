3b167aea23b3edba360f7c032622cac96c4af944
Lance Craig <lancra@outlook.com>
2025-03-05 06:30:26 -0500

    pwsh: add schema for alias definitions

--- .local/bin/powershell/profile/setup-aliases.ps1
+++ .local/bin/powershell/profile/setup-aliases.ps1
@@ -2,6 +2,7 @@ Import-Module HackF5.ProfileAlias
 Get-Content -Path "$env:XDG_CONFIG_HOME/powershell/aliases.json" |
     ConvertFrom-Json |
     ForEach-Object { $_.PSObject.Properties } |
+    Where-Object { -not $_.Name.StartsWith('$') } |
     ForEach-Object {
         if ($_.Value.bash) {
             Set-ProfileAlias -Name $_.Name -Command $_.Value.command -Bash -Force | Out-Null
