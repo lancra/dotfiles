e0f7938fb9a35758d3956ff165e6874244f9452f
Lance Craig <lancra@outlook.com>
2025-03-05 06:39:05 -0500

    pwsh: add dotnet alias
    Includes support for tab completion on the alias.

--- .local/bin/powershell/profile/setup-dotnet-completion.ps1
+++ .local/bin/powershell/profile/setup-dotnet-completion.ps1
@@ -1,7 +1,10 @@
 # PowerShell parameter completion shim for the dotnet CLI
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-        dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+@('dotnet', 'd') |
+    ForEach-Object {
+        Register-ArgumentCompleter -Native -CommandName $_ -ScriptBlock {
+            param($wordToComplete, $commandAst, $cursorPosition)
+                dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+                    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+                }
         }
-}
+    }
