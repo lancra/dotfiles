4a7ee55923c0b637577a84dbfc2641574dc91b57
Lance Craig <lancra@outlook.com>
2025-04-16 05:50:27 -0400

    pwsh: align sizes in file size output

--- .local/bin/file-size.ps1
+++ .local/bin/file-size.ps1
@@ -5,9 +5,29 @@ param(
     [string] $Path = '.'
 )
 
+class FileSize {
+    [decimal] $Count
+    [string] $Denomination
+    [string] $Path
+
+    FileSize([decimal] $count, [string] $denomination, [string] $path) {
+        $this.Count = $count
+        $this.Denomination = $denomination
+        $this.Path = $path
+    }
+
+    [void] Print() {
+        $displayCount = ('{0:N2}' -f $this.Count).PadLeft(6)
+        $displayDenomination = $this.Denomination.PadLeft(2)
+
+        Write-Host "$displayCount $displayDenomination " -ForegroundColor Yellow -NoNewline
+        Write-Host $this.Path
+    }
+}
+
 function Get-Size {
     [CmdletBinding()]
-    [OutputType([string])]
+    [OutputType([FileSize])]
     param(
         [Parameter(Mandatory)]
         [string] $Path
@@ -15,7 +35,7 @@ function Get-Size {
     begin {
         function Format-Size {
             [CmdletBinding()]
-            [OutputType([string])]
+            [OutputType([FileSize])]
             param(
                 [Parameter(Mandatory)]
                 [long] $Count,
@@ -28,7 +48,7 @@ function Get-Size {
             )
             process {
                 $isLargestDenomination = $Count -ge $DenominationCount -or $DenominationCount -eq 1
-                return $isLargestDenomination ? "{0:N2} $Denomination" -f ($Count / $DenominationCount) : $null
+                return $isLargestDenomination ? [FileSize]::new($Count / $DenominationCount, $Denomination, $Path) : $null
             }
         }
     }
@@ -48,14 +68,14 @@ $targetPaths = @($Path)
 if ($null -eq $item) {
     throw 'The provided path is invalid.'
 } elseif ($item -is [System.IO.DirectoryInfo]) {
+    $absolutePath = Resolve-Path -Path $Path
     $targetPaths = Get-ChildItem -Path $item.FullName -File |
-        Select-Object -ExpandProperty FullName
+        Select-Object -ExpandProperty FullName |
+        ForEach-Object { [System.IO.Path]::GetRelativePath($absolutePath, $_) }
 }
 
 $targetPaths |
     ForEach-Object {
         $size = Get-Size -Path $_
-
-        Write-Host $size.PadLeft(9) -ForegroundColor Yellow -NoNewline
-        Write-Host " $_"
+        $size.Print()
     }
