f0f9d9f7d4347f043f74b6569fd1f1fcf668c6a6
Lance Craig <lancra@outlook.com>
2025-09-18 06:38:56 -0400

    git: add script to prune orphan notes
    Include PowerShell aliases to check the operation and to perform it.

---
+++ .local/bin/git/prune-orphan-notes.ps1
@@ -0,0 +1,47 @@
+<#
+.SYNOPSIS
+Prunes notes from a repository where the commit is not found on any branch.
+
+.DESCRIPTION
+Retrieves the list of notes for the repository. Then, for each commit, checks
+whether any remove or local branch contains the commit. If this check passes,
+the note is retained. Otherwise, the note is removed.
+
+.PARAMETER Repository
+The repository to operate against. The current directory is used when no value
+is provided.
+
+.PARAMETER Check
+Displays the operation for each commit, either retaining or removing, instead of
+deleting any commits.
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [string] $Repository = $PWD,
+
+    [switch] $Check
+)
+
+$repositoryRoot = git -C $Repository rev-parse --show-toplevel 2> $null
+if ($LASTEXITCODE -eq 128) {
+    throw "The provided path '$Repository' is not a Git repository."
+}
+
+git -C $repositoryRoot notes list |
+    ForEach-Object {
+        $noteIds = $_ -split ' '
+        $commitId = $noteIds[1]
+        $inBranch = $null -ne (git -C $repositoryRoot branch --all --contains $commitId)
+
+        if ($Check) {
+            $prefix = $inBranch ? 'RETAIN' : 'REMOVE'
+            $colorCode = $inBranch ? 32 : 31
+            Write-Output "`e[${colorCode}m$prefix $commitId`e[39m"
+        } elseif (-not $inBranch) {
+            git -C $repositoryRoot notes remove $commitId
+        } else {
+            Write-Output "Retaining note for object $commitId"
+        }
+    }
