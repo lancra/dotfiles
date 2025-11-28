3078d615b3a8aca1f76c039a47391f5814c50047
Lance Craig <lancra@outlook.com>
2025-11-28 13:31:21 -0500

    vscode: generate settings from scoped files

---
+++ .local/bin/vscode/generate-settings.ps1
@@ -0,0 +1,100 @@
+[CmdletBinding()]
+param()
+
+$configurationDirectory = Join-Path -Path $env:XDG_CONFIG_HOME -ChildPath 'vscode'
+$sourceDirectory = Join-Path -Path $configurationDirectory -ChildPath 'settings'
+$targetPath = Join-Path -Path $configurationDirectory -ChildPath 'settings.json'
+
+$generators = @{
+    'mssql.connections.json' = "$env:HOME/.local/bin/vscode/generate-mssql-connections-settings.ps1"
+}
+
+$schemaProperty = '$schema'
+function Add-SettingsProperty {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [pscustomobject] $Target,
+
+        [Parameter(Mandatory)]
+        [pscustomobject] $Source,
+
+        [Parameter()]
+        [string] $Accessor
+    )
+    process {
+        $targetReference = '$Target'
+        $Source.PSObject.Properties |
+            Where-Object -Property Name -NE $schemaProperty |
+            ForEach-Object {
+                $propertyName = $_.Name
+                $propertyAccessor = "$Accessor.'$propertyName'"
+                $targetPropertyValue = "$targetReference$propertyAccessor" |
+                    Invoke-Expression
+                if ($null -eq $targetPropertyValue) {
+                    $propertyValue = '$_.Value'
+                    "$targetReference$Accessor | Add-Member -MemberType NoteProperty -Name '$propertyName' -Value $propertyValue" |
+                        Invoke-Expression
+                } elseif ($_.Value -is [pscustomobject]) {
+                    Add-SettingsProperty -Target $Target -Source $_.Value -Accessor $propertyAccessor
+                } else {
+                    Write-Output "`e[31mDuplicate definitions found for `"$propertyAccessor`".`e[39m"
+                }
+            }
+    }
+}
+
+function New-SortedSettings {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [pscustomobject] $Target,
+
+        [Parameter(Mandatory)]
+        [pscustomobject] $Source,
+
+        [Parameter()]
+        [string] $Accessor
+    )
+    process {
+        $targetReference = '$Target'
+        $initialValue = '([PSCustomObject]@{})'
+        $Source.PSObject.Properties |
+            Sort-Object -Property Name |
+            ForEach-Object {
+                $propertyName = $_.Name
+
+                if ($_.Value -is [pscustomobject]) {
+                    "$targetReference$Accessor | Add-Member -MemberType NoteProperty -Name '$propertyName' -Value $initialValue" |
+                        Invoke-Expression
+                    New-SortedSettings -Target $Target -Source $_.Value -Accessor "$Accessor.'$propertyName'"
+                } else {
+                    $propertyValue = '$_.Value'
+                    "$targetReference$Accessor | Add-Member -MemberType NoteProperty -Name '$propertyName' -Value $propertyValue" |
+                        Invoke-Expression
+                }
+            }
+    }
+}
+
+$aggregateSettings = [pscustomobject]@{}
+Get-ChildItem -Path $sourceDirectory |
+    ForEach-Object {
+        if ($_.Name.EndsWith('.settings.json')) {
+            $settings = Get-Content -Path $_.FullName |
+                ConvertFrom-Json
+        } else {
+            $generator = $generators[$_.Name]
+            $settings = & $generator -Source $_.FullName |
+                ConvertFrom-Json
+        }
+
+        Add-SettingsProperty -Target $aggregateSettings -Source $settings
+    }
+
+$sortedSettings = [pscustomobject]@{}
+New-SortedSettings -Target $sortedSettings -Source $aggregateSettings
+
+$sortedSettings |
+    ConvertTo-Json -Depth 3 |
+    Set-Content -Path $targetPath
