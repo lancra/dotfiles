353741bfaf30f83f963a7f5179cfdde7035d4326
Lance Craig <lancra@outlook.com>
2025-12-02 04:41:22 -0500

    software: add vscode extensions export

---
+++ .local/bin/software/vscode/export-extensions.ps1
@@ -0,0 +1,26 @@
+using module ../software.psm1
+
+[CmdletBinding()]
+param()
+
+$exportId = & "$env:HOME/.local/bin/software/get-export-id-from-path.ps1"
+
+Get-Content -Path "$env:HOME/.vscode/extensions/extensions.json" |
+    ConvertFrom-Json |
+    ForEach-Object {
+        $identifier = $_.identifier
+
+        $directoryPath = $_.location.path.TrimStart('/')
+        $manifestPath = Join-Path -Path $directoryPath -ChildPath '.vsixmanifest'
+        $manifest = [xml](Get-Content -Path $manifestPath)
+        $manifestMetadata = $manifest.PackageManifest.Metadata
+
+        $id = [InstallationId]::new($identifier.id, $exportId)
+        $metadata = [ordered]@{
+            Uuid = $identifier.uuid
+            DisplayName = $manifestMetadata.DisplayName
+            Description = $manifestMetadata.Description.InnerText
+        }
+
+        [Installation]::new($id, $metadata)
+    }
