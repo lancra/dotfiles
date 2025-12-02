8736c742ef0c5e2fe10ce1e5bc22ffc0111d9cf0
Lance Craig <lancra@outlook.com>
2025-12-02 06:08:36 -0500

    pwsh: use bin variable for profile setup

--- .config/powershell/core.profile.ps1
+++ .config/powershell/core.profile.ps1
@@ -1,5 +1,5 @@
 Import-Module 'Lance'
-Get-ChildItem -Path "$env:HOME/.local/bin/powershell/profile" -Filter '*.ps1' |
+Get-ChildItem -Path "$env:BIN/powershell/profile" -Filter '*.ps1' |
     ForEach-Object {
         . $_
     }
--- .config/powershell/windows.profile.ps1
+++ .config/powershell/windows.profile.ps1
@@ -1,1 +1,1 @@
-. "$env:HOME/.local/bin/powershell/profile/setup-oh-my-posh.ps1"
+. "$env:BIN/powershell/profile/setup-oh-my-posh.ps1"
