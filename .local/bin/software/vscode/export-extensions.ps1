4fabe4c330007b89cb02926a895642ee83520f6d
Lance Craig <lancra@outlook.com>
2025-12-12 04:33:29 -0500

    software: ignore duplicate vscode extensions

--- .local/bin/software/vscode/export-extensions.ps1
+++ .local/bin/software/vscode/export-extensions.ps1
@@ -5,10 +5,18 @@ param()
 
 $exportId = & "$env:BIN/software/get-export-id-from-path.ps1"
 
+$uniqueIds = @()
 Get-Content -Path "$env:HOME/.vscode/extensions/extensions.json" |
     ConvertFrom-Json |
     ForEach-Object {
         $identifier = $_.identifier
+
+        # Extensions seem to be duplicated due to a pending updates.
+        if ($uniqueIds -contains $identifier.id) {
+            return
+        }
+
+        $uniqueIds += $identifier.id
 
         $directoryPath = $_.location.path.TrimStart('/')
         $manifestPath = Join-Path -Path $directoryPath -ChildPath '.vsixmanifest'
