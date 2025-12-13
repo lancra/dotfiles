1beb3f3e32f9cb9bb90d1a0748b4e86e08b2a54f
Lance Craig <lancra@outlook.com>
2025-12-13 07:39:38 -0500

    git: simplify alias generation with other scripts

--- .local/bin/git/generate-aliases.ps1
+++ .local/bin/git/generate-aliases.ps1
@@ -20,39 +20,16 @@ $builder = [System.Text.StringBuilder]::new()
 $aliases = Get-Content -Path $sourcePath |
     ConvertFrom-Json
 
-$variables = @{}
-$aliases.variables.PSObject.Properties |
-    ForEach-Object {
-        $variables[$_.Name] = $_.Value
-    }
+$variables = & "$env:BIN/powershell/convert-to-hashtable.ps1" -Object ($aliases |
+    Select-Object -ExpandProperty 'variables')
 
-$tokenGroupKey = 'token'
-$variablePattern = "(?<$tokenGroupKey>__.*?__)"
 $aliasCount = 0
 $aliases.definitions.PSObject.Properties |
     ForEach-Object {
-        [PSCustomObject]@{
-            Key = $_.Name
-            Body = $_.Value.body
-        }
-    } |
-    ForEach-Object {
-        $body = $_.Body
-        Select-String -InputObject $_.Body -Pattern $variablePattern -AllMatches |
-            Select-Object -ExpandProperty Matches |
-            ForEach-Object {
-                $tokenGroup = $_.Groups |
-                    Where-Object -Property Name -EQ $tokenGroupKey
-                $token = $tokenGroup.Value.Trim('_')
-                $value = $variables[$token]
-                if ($null -eq $value) {
-                    throw "No value specified for $token variable."
-                }
+        $key = $_.Name
+        $body = & "$env:BIN/replace-tokens.ps1" -Text $_.Value.body -Token $variables
 
-                $body = $body.Replace($tokenGroup.Value, $value)
-            }
-
-        [void]$builder.Append("$([System.Environment]::NewLine)`t$($_.Key) = `"$body`"")
+        [void]$builder.Append("$([System.Environment]::NewLine)`t$key = `"$body`"")
         $aliasCount++
     }
 
