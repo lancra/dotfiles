46eb95e482a2f1d9a34c04aadd88dff4d8cb41b6
Lance Craig <lancra@outlook.com>
2025-12-17 04:55:44 -0500

    pwsh: copy help parameters to extended help script

--- .local/bin/get-extended-help.ps1
+++ .local/bin/get-extended-help.ps1
@@ -9,22 +9,54 @@ Otherwise, help is provided using the normal mechanism.
 .PARAMETER Name
 Gets help about the specified command or alias.
 
+.PARAMETER Detailed
+Adds parameter descriptions and examples to the basic help display.
+
+.PARAMETER Full
+Displays the entire help article for a cmdlet. Full includes parameter
+descriptions and attributes, examples, input and output object types, and
+additional notes.
+
+.PARAMETER Parameter
+Displays only the detailed descriptions of the specified parameters. Wildcards
+are permitted.
+
+.LINK
+Issue: https://github.com/PowerShell/PowerShell/issues/2899
+
 .NOTES
 Provides a workaround for PowerShell not resolving alias definitions unless the
 associated script is located in the PATH.
-
-See: https://github.com/PowerShell/PowerShell/issues/2899
 #>
-
-[CmdletBinding()]
+# NOTE: A non-breaking space is used between the key and URI in the .LINK
+#       section to avoid duplication in the Get-Help output.
+#       See: https://redirect.github.com/PowerShell/PowerShell/issues/24504
+[CmdletBinding(DefaultParameterSetName = 'Default')]
 param(
-    [Parameter(Mandatory)]
-    [string] $Name
+    [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+    [string] $Name,
+
+    [Parameter(ParameterSetName = 'Detailed')]
+    [switch] $Detailed,
+
+    [Parameter(ParameterSetName = 'Full')]
+    [switch] $Full,
+
+    [Parameter(Mandatory, ParameterSetName = 'Parameter')]
+    [string] $Parameter
 )
 
 $alias = Get-Alias -Name $Name -ErrorAction SilentlyContinue
-if ($alias) {
-    Get-Help -Name $alias.Definition
-} else {
-    Get-Help -Name $Name
+$arguments = @{
+    Name = $alias ? $alias.Definition : $Name
 }
+
+if ($PSCmdlet.ParameterSetName -eq 'Detailed') {
+    $arguments['Detailed'] = $Detailed
+} elseif ($PSCmdlet.ParameterSetName -eq 'Full') {
+    $arguments['Full'] = $Full
+} elseif ($PSCmdlet.ParameterSetName -eq 'Parameter') {
+    $arguments['Parameter'] = $Parameter
+}
+
+Get-Help @arguments
