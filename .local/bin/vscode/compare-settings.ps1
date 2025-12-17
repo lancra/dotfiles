ad5fea23f2b87ba56e8fdaef7de4939b93782cdf
Lance Craig <lancra@outlook.com>
2025-12-16 20:52:04 -0500

    vscode: denote files during settings comparison

--- .local/bin/vscode/compare-settings.ps1
+++ .local/bin/vscode/compare-settings.ps1
@@ -3,23 +3,39 @@ param(
     [switch] $Unsorted
 )
 
-$sourceSettingsPath = "$env:XDG_CONFIG_HOME/vscode/settings.json"
-$settingsPath = $sourceSettingsPath
-if (-not $Unsorted) {
-    $settings = Get-Content -Path $settingsPath |
-        ConvertFrom-Json
-
-    $settingsPath = "$([System.IO.Path]::GetTempFileName()).json"
-    & "$env:BIN/vscode/sort-settings-object.ps1" -Settings $settings |
-        Set-Content -Path $settingsPath
+$timestamp = [datetime]::Now.ToString('HHmmss')
+function New-TemporarySettingsPath {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory)]
+        [string] $Name
+    )
+    process {
+        $temporaryDirectory = [System.IO.Path]::GetTempPath()
+        $fileName = "$Name-vscode-settings.$timestamp.json"
+        $path = Join-Path -Path $temporaryDirectory -ChildPath $fileName
+        $path -replace '\\', '/'
+    }
 }
 
-$generatedPath = "$([System.IO.Path]::GetTempFileName()).json"
+$sourceSettingsPath = "$env:XDG_CONFIG_HOME/vscode/settings.json"
+$livePath = $sourceSettingsPath
+if (-not $Unsorted) {
+    $settings = Get-Content -Path $livePath |
+        ConvertFrom-Json
+
+    $livePath = New-TemporarySettingsPath -Name 'live'
+    & "$env:BIN/vscode/sort-settings-object.ps1" -Settings $settings |
+        Set-Content -Path $livePath
+}
+
+$generatedPath = New-TemporarySettingsPath -Name 'generated'
 
 & "$env:BIN/vscode/generate-settings.ps1" -Path $generatedPath
-git diff --no-index $generatedPath $settingsPath
+git diff --no-index $generatedPath $livePath
 
 Remove-Item -Path $generatedPath
 if (-not $Unsorted) {
-    Remove-Item -Path $settingsPath
+    Remove-Item -Path $livePath
 }
