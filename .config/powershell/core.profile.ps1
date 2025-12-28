e32a8d9b55682a52fc7de8438c542049419ea077
Lance Craig <lancra@outlook.com>
2025-12-28 08:43:33 -0500

    pwsh: move completion scripts to sub-directory

--- .config/powershell/core.profile.ps1
+++ .config/powershell/core.profile.ps1
@@ -1,5 +1,5 @@
 Import-Module 'Lance'
-Get-ChildItem -Path "$env:BIN/powershell/profile" -Filter '*.ps1' |
+Get-ChildItem -Path "$env:BIN/powershell/profile" -Filter '*.ps1' -Recurse |
     ForEach-Object {
         . $_
     }
