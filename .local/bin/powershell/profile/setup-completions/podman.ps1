d425be85420fd662fe9ee1f0fdefd5633d4f44e7
Lance Craig <lancra@outlook.com>
2025-12-28 09:28:45 -0500

    pwsh: remove podman alias completion customization
    The pm alias uses the tab completions for the underlying command, so
    customizing the setup is unnecessary.

--- .local/bin/powershell/profile/setup-completions/podman.ps1
+++ .local/bin/powershell/profile/setup-completions/podman.ps1
@@ -1,3 +1,1 @@
-$setupCompletion = & podman completion powershell | Out-String
-$setupCompletion += "`nRegister-ArgumentCompleter -CommandName 'pm' -ScriptBlock `${__podmanCompleterBlock}"
-$setupCompletion | Invoke-Expression
+& podman completion powershell | Out-String | Invoke-Expression
