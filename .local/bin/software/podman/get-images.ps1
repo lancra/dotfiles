9ae08990deb31a3bb731dbcdac0e1d4aa2961091
Lance Craig <lancra@outlook.com>
2026-04-02 06:12:00 -0400

    software: expand podman ignore rules
    - Use patterns instead of static Boolean expressions for statically
      ignored repositories.
    - Ignore dangling images (i.e. "<none>").
    - Add local ignore file for repositories.

--- .local/bin/software/podman/get-images.ps1
+++ .local/bin/software/podman/get-images.ps1
@@ -1,13 +1,37 @@
 [CmdletBinding()]
 param()
 
+$ignoredRepositories = @()
+$ignoredRepositoriesPath = "$env:XDG_CONFIG_HOME/software/.ignored.podman-images.json"
+if (Test-Path -Path $ignoredRepositoriesPath) {
+    $ignoredRepositories = Get-Content -Path $env:XDG_CONFIG_HOME/software/.ignored.podman-images.json |
+        ConvertFrom-Json
+}
+
+$repositoryIgnorePatterns = @(
+    '<none>',
+    'localhost\/.*'
+)
+
 & podman images --format '{{ json . }}' |
     ForEach-Object {
         $_ |
             ConvertFrom-Json
     } |
     ForEach-Object {
-        if ($_.repository.StartsWith('localhost/')) {
+        if ($ignoredRepositories -contains $_.repository) {
+            return
+        }
+
+        $ignoreRepositoryFromPattern = $false
+        foreach ($pattern in $repositoryIgnorePatterns) {
+            if ($_.repository -match $pattern) {
+                $ignoreRepositoryFromPattern = $true
+                break
+            }
+        }
+
+        if ($ignoreRepositoryFromPattern) {
             return
         }
 
