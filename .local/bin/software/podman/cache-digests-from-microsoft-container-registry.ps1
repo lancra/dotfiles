43a27b046b8f55fe737464a7a4fd0d9aae7054f1
Lance Craig <lancra@outlook.com>
2026-04-03 06:06:31 -0400

    software: support oci digests for mcr podman cache
    Some images uploaded to the Microsoft Container Registry do not have an
    available Docker manfiest, and instead expose the OCI image index. When
    this occurs, the caching process now falls back to the latter and
    calculates the digest from the response using sha256sum.
    
    Attempts were made to utilize the Get-FileHash cmdlet to avoid the Git
    tools dependency, but PowerShell's insistence on adding newlines or
    replacing Unix newlines prevented an easy solution.

--- .local/bin/software/podman/cache-digests-from-microsoft-container-registry.ps1
+++ .local/bin/software/podman/cache-digests-from-microsoft-container-registry.ps1
@@ -4,26 +4,73 @@ param(
     [string] $Repository
 )
 
+function Get-RepositoryTagDigest {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string] $Url
+    )
+    process {
+        $getDockerManifestCommandArguments = @(
+            'curl',
+            '--silent',
+            "--header 'Accept: application/vnd.docker.distribution.manifest.v2+json'",
+            "--write-out '{`"metadata`":%{json},`"headers`":%{header_json}}'",
+            "--out-null",
+            $Url
+        )
+        $getDockerManifestCommand = [scriptblock]::Create("$getDockerManifestCommandArguments")
+        $dockerManifestResponse = Invoke-Command -ScriptBlock $getDockerManifestCommand |
+            ConvertFrom-Json
+
+        $dockerManifestStatusCode = $dockerManifestResponse |
+            Select-Object -ExpandProperty 'metadata' |
+            Select-Object -ExpandProperty 'http_code'
+        if ($dockerManifestStatusCode -eq 200) {
+            return $dockerManifestResponse |
+                Select-Object -ExpandProperty 'headers' |
+                Select-Object -ExpandProperty 'docker-content-digest'
+        }
+
+        $responsePath = "$([System.IO.Path]::GetTempFileName()).json"
+        $getOciImageIndexCommandArguments = @(
+            'curl',
+            '--silent',
+            "--header 'Accept: application/vnd.oci.image.index.v1+json'",
+            "--output '$responsePath'",
+            $Url
+        )
+
+        $getOciImageIndexCommand = [scriptblock]::Create("$getOciImageIndexCommandArguments")
+        Invoke-Command -ScriptBlock $getOciImageIndexCommand
+
+        $digestResponse = & sha256sum $responsePath
+        Remove-Item -Path $responsePath
+
+        $digestGroupName = 'digest'
+        $digest = $digestResponse |
+            Select-String -Pattern "\\(?<$digestGroupName>.*?) \*.*" |
+            Select-Object -ExpandProperty Matches |
+            Select-Object -ExpandProperty Groups |
+            Where-Object -Property Name -EQ $digestGroupName |
+            Select-Object -ExpandProperty Value
+        return "sha256:$digest"
+    }
+}
+$getRepositoryTagDigestFunction = ${function:Get-RepositoryTagDigest}.ToString()
+
 $tagsByDigest = @{}
 $tagProperties = @(
-    @{ Name = 'Tag'; Expression = { $_} },
-    @{ Name = 'Url'; Expression = { "https://mcr.microsoft.com/v2/$Repository/manifests/$_"} }
+    @{ Name = 'Tag'; Expression = { $_ } },
+    @{ Name = 'Url'; Expression = { "https://mcr.microsoft.com/v2/$Repository/manifests/$_" } }
 )
 & curl --silent "https://mcr.microsoft.com/v2/$Repository/tags/list" |
     ConvertFrom-Json |
     Select-Object -ExpandProperty 'tags' |
     Select-Object -Property $tagProperties |
     ForEach-Object -Parallel {
-        $digestCommandArguments = @(
-            'curl',
-            '--silent',
-            "--header 'Accept: application/vnd.docker.distribution.manifest.v2+json'",
-            "--write-out '%header{Docker-Content-Digest}'",
-            "--out-null",
-            $_.Url
-        )
-        $digestCommand = [scriptblock]::Create("$digestCommandArguments")
-        $digest = Invoke-Command -ScriptBlock $digestCommand
+        ${function:Get-RepositoryTagDigest} = $using:getRepositoryTagDigestFunction
+        $digest = Get-RepositoryTagDigest -Url $_.Url
 
         [pscustomobject]@{
             Tag = $_.Tag
